6e23d30560ae72aaf09e02bcb5de4fd15924a0c3
PunitLodha <punitlodha@pm.me>
2021-08-25 13:24:47 +0530

    up

--- windows/rust.bat
+++ windows/rust.bat
@@ -2,5 +2,5 @@ cd ..\src\rust
 rustup install stable-i686-pc-windows-gnu
 set CARGO_TARGET_DIR = "..\..\windows" && rustup run stable-i686-pc-windows-gnu cargo build
 cd ..\..\windows
-dir
+dir .\debug
 copy debug\ccx_rust.lib .\ccx_rust.lib
