b310bea48dfb2848f63bd67423abac53118b0766
PunitLodha <punitlodha@pm.me>
2021-08-25 13:16:56 +0530

    update sln

--- windows/rust.bat
+++ windows/rust.bat
@@ -1,5 +1,6 @@
 cd ..\src\rust
-set CARGO_TARGET_DIR = "..\..\windows" && cargo build
+rustup install stable-i686-pc-windows-gnu
+set CARGO_TARGET_DIR = "..\..\windows" && rustup run stable-i686-pc-windows-gnu cargo build
 cd ..\..\windows
 dir
 copy debug\ccx_rust.lib .\ccx_rust.lib
