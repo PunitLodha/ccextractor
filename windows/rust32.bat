5385f79326bc9f4868495e3a5e2b410239e97f46
PunitLodha <punitlodha@pm.me>
2021-10-01 19:55:02 +0530

    update

--- windows/rust32.bat
+++ windows/rust32.bat
@@ -2,4 +2,4 @@ cd ..\src\rust
 set CARGO_TARGET_DIR = "..\..\windows" && cargo build --target=i686-pc-windows-msvc
 cd ..\..\windows
 dir
-copy debug\ccx_rust.lib .\ccx_rust.lib
+copy i686-pc-windows-msvc\ccx_rust.lib .\ccx_rust.lib
