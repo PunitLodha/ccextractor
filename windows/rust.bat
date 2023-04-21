295c65610cfaebf449c5e504afc302e0d9c5e5c7
Punit Lodha <punitlodha@pm.me>
2023-04-21 23:49:27 +0200

    Add option for release builds in windows

--- windows/rust.bat
+++ windows/rust.bat
@@ -1,6 +1,10 @@
 for /f "delims=" %%i in ('cd') do set output=%%i
 set CARGO_TARGET_DIR=%output%
+set profile=
+if [%1]==[] goto :build
+if %1==release set profile=--release
+:build
 cd ..\src\rust
-cargo build
+cargo build %profile%
 cd ..\..\windows
 copy debug\ccx_rust.lib .\ccx_rust.lib
